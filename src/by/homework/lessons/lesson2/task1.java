87d2ca97efd1ab24285303a1b37a8450713131af
Litvia <litvinenkoayu@gmail.com>
2025-02-13 22:59:40 +0300

    First packages created

--- src/by/homework/lessons/lesson2/task1.java
+++ src/by/homework/lessons/lesson2/task1.java
@@ -1,4 +1,9 @@
 package by.homework.lessons.lesson2;
 
 public class task1 {
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Lesson2. February 13, 2025");
+        }
+    }
 }
--- src/by/homework/lessons/lesson3/task1.java
+++ src/by/homework/lessons/lesson3/task1.java
@@ -1,4 +1,9 @@
 package by.homework.lessons.lesson3;
 
 public class task1 {
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Lesson3. February 17, 2025");
+        }
+    }
 }

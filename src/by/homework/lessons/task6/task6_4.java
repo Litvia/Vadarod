f8a264c690a18dec07f85ca07f96135964908788
Litvia <litvinenkoayu@gmail.com>
2025-03-02 22:22:27 +0300

    добавила домашнее задание номер 6

---
+++ src/by/homework/lessons/task6/task6_4.java
@@ -0,0 +1,8 @@
+package by.homework.lessons.task6;
+
+public class task6_4 {
+//    4.*.Создать массив (вручную). Отсортировать элементы массива в порядке возрастания. Вывести полученный массив. Выполнить с
+//    помощью цикла for
+    //    public static void main(String[] args) {
+//    }
+}
